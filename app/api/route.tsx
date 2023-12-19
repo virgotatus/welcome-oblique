70223bbe5042cc7e576588a2376aa4f6b60f4792
virgotatus <435491723@qq.com>
2023-12-19 19:29:39 +0800

    tally find option index

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -3,6 +3,21 @@ import sendEmail from "@/app/send-email/sendEmail";
 import Chat from "@/app/_gpt/openaiChat";
 import moment from "moment";
 
+const PlaceID = [
+  "0a8c5b4e-18a1-4808-bf8b-c56aa5e544dc",
+  "ac7c13c3-266b-41d4-91e8-7f085cf528df",
+  "78d1115d-0a06-4525-8ce1-a000f6edfbe0",
+];
+
+function getIndex(value: string, arr: string[]) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+  return -1; //to handle the case where the value doesn't exist
+}
+
 const Places = ["猛虎镇", "赛鸽镇", "兔子洞"];
 
 interface TallyForm {
@@ -29,8 +44,10 @@ function parseTally(requestBody: any): TallyForm {
   const username = fields[0].value;
   const question = fields[1].value;
   const email = fields[2].value;
-  const place = Places[fields[3].value]; // fiels[3]? 0~1
-  const obj = fields[4].value;
+  const place_idx = getIndex(fields[3].value[0], PlaceID);
+  const place = Places[place_idx]; // fiels[3]? 0~1
+  const obj = fields[4 + place_idx].value;
+  console.log(username, email, question, place, obj);
   return {
     username: username,
     question: question,
@@ -50,7 +67,7 @@ export async function POST(request: NextRequest) {
     place: tally.place,
   });
   const AIres: AIResult = { query: tally, answer: result, oblique: oblique };
-  console.log(tally.username, result);
+  console.log(tally.question, result);
   const info = await sendEmail(AIres);
   console.log("Message sent: %s", info);
   return NextResponse.json({ status: { status } });
